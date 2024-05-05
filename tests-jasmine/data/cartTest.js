7e209cf6fc4a4da38dfcf710adb19327cdeb894a
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-05 18:16:33 +0200

    Version 4.6.3

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -2,11 +2,11 @@ import { addToCart,cart,loadFromStorage } from "../../data/cart.js";
 
 describe('test suite: addToCart',()=>{
   it('Added to cart case',()=>{
-    spyOn(localStorage, 'getItem').and.callFake(()=>{
+    spyOn(localStorage, 'getItem').and.callFake(()=>{  //creates copy of localstorage
       return JSON.stringify([]);
     });
     loadFromStorage();
-    
+
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
   });
