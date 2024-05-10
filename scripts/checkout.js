f69c7881556154f785af40e040e90b53ae77dbe0
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-10 12:27:49 +0200

    Version 4.7.8(added class to product, changed amazonjs/ordersummaryjs)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,8 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeder.js";
 
-import "../data/cart-oop.js"
-import "../data/cart-class.js"
+// import "../data/cart-oop.js"
+// import "../data/cart-class.js"
 
 renderCheckoutHeader();
 renderOrderSummary();
