b705a0931f8a9210460927e747ce641218999392
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-10 10:16:27 +0200

    Version 4.7.5(added oop cart file)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeder.js";
-//
+import "../data/cart-oop.js"
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; //dayjs library
 
 renderCheckoutHeader();
