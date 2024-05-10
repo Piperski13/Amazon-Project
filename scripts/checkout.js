f4ef544e4f5e0995620f43e01d63afb99b2e1180
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-10 10:44:47 +0200

    Version 4.7.6(added class cart file)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,10 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeder.js";
+
 import "../data/cart-oop.js"
-import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; //dayjs library
+import "../data/cart-class.js"
 
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
-
-
-
-// const body = document.body;
-// body.addEventListener('click',(event)=>{
-//   if(!event.target.matches('span') && !event.target.matches('input')){
-//     renderOrderSummary();
-//   }
-// });
