fc53655c8774cf9dc6517057b02909ab98289584
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-11 16:22:20 +0200

    Version 4.8.2 (added car.js for practicing oop/classes)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeder.js";
+import '../data/car.js';
 
 // import "../data/cart-oop.js"
 // import "../data/cart-class.js"
