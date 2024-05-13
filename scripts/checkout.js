19caf1f33f87ae201754708f7605af9c61b66eed
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-13 08:46:07 +0200

    Version 4.9(promise practice)

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,52 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeder.js";
-import {loadProducts} from "../data/products.js"
+import { loadProducts } from "../data/products.js"
+import { loadCart } from "../data/cart.js"
 
 // import '../data/backend-practice.js';
 // import '../data/car.js';
 // import "../data/cart-oop.js"
 // import "../data/cart-class.js"
-loadProducts(()=>{
+
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('value test');
+    });
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+]).then((values)=>{
+  console.log(values);
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 })
+
+// new Promise((resolve)=>{
+//   loadProducts(()=>{
+//     resolve();
+//   });
+// }).then(()=>{
+//   return new Promise((resolve)=>{
+//     loadCart(()=>{
+//       resolve();
+//     });
+//   });
+// }).then(()=>{
+//   renderCheckoutHeader();
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
+
+// loadProducts(()=>{
+//   loadCart(()=>{
+//     renderCheckoutHeader();
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
