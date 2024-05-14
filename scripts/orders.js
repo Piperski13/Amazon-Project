405887c0f875a7e22217bf1d8798cb20fd3ad51b
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-14 13:33:38 +0200

    Version 4.9.8(generated html for orders with backend)

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,10 +1,60 @@
 import {orders} from '../data/orders.js';
 import {formatCurrency} from './utils/money.js';
 import {renderDateOrder} from './utils/date.js';
+import {loadProducts,products} from '../data/products.js';
+
+loadProducts(renderOrderSummary);
 
 function renderOrderSummary(){
   let generatedHTML = '';
+  let generatedDetailsHTML = '';
+
   orders.forEach(order => {
+
+    order.products.forEach(productOrder =>{
+      let matchingProduct;
+
+      products.forEach(product => {
+        if(product.id === productOrder.productId){
+          matchingProduct = product;
+        }
+      });
+     
+      generatedDetailsHTML += `
+          <div class="order-details-grid">
+            <div class="product-image-container">
+              <img src=${matchingProduct.image}>
+            </div>
+
+            <div class="product-details">
+              <div class="product-name">
+                ${matchingProduct.name}
+              </div>
+              <div class="product-delivery-date">
+                Arriving on: ${renderDateOrder(productOrder.estimatedDeliveryTime)}
+              </div>
+              <div class="product-quantity">
+                Quantity: ${productOrder.quantity}
+              </div>
+              <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+              </button>
+            </div>
+
+            <div class="product-actions">
+              <a href="tracking.html?orderid=123&productId=456">
+                <button class="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+          </div>
+      `;
+    });
+    function returnGeneratedDetails(generatedDetailsHTML){
+      return generatedDetailsHTML;
+    }
     generatedHTML += `
         <div class="order-container">
           <div class="order-header">
@@ -24,70 +74,16 @@ function renderOrderSummary(){
               <div>${order.id}</div>
             </div>
           </div>
-
-          <div class="order-details-grid">
-            <div class="product-image-container">
-              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 15
-              </div>
-              <div class="product-quantity">
-                Quantity: 1
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html?orderid=123&productId=456">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-
-            <div class="product-image-container">
-              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 19
-              </div>
-              <div class="product-quantity">
-                Quantity: 2
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-          </div>
+          ${returnGeneratedDetails(generatedDetailsHTML)}
         </div>
     `;
+    generatedDetailsHTML = '';
   });
   document.querySelector('.js-orders-grid').innerHTML = generatedHTML;
-}
-renderOrderSummary()
-console.log(orders); // number of orders
-console.log(orders[1].products); //index 1 all products array
+};
+
+// console.log(orders); // number of orders
+// console.log(orders[1].products); //index 1 all products array
+// console.log(orders[0].products[0].productId);
+
 
